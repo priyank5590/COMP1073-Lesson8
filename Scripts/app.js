cb4e8e20fe45d6d45ca1bf19719fdce947a1526c
priyank5590 <vyaspriyank5@gmail.com>
2016-03-10 12:18:03 -0800

    change in app.js file

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,45 +3,6 @@
 // setup your IIFE (Immediately Invoked Function Expression)
 (function () {
 
-	if (document.getElementById("about") != null) {
-		console.log("About Page");
-			
-			var naruto;
-			naruto = document.getElementById("naruto");
-			naruto.innerHTML="This was my final project in the first semester for web and internet fundamentels. Perticularly in this project I learned how to use jQuery slider and integrate google maps on website. " 
-		
-
-	} else if (document.getElementById("projects") != null) {
-		console.log("Project Page");
-
-	} else if (document.getElementById("contact") != null) {
-		console.log("Contact Page");
-
-		var email = document.getElementById("email");
-		var password = document.getElementById("password");
-		var checkMeOut = document.getElementById("checkMeOut");
-		var submitButton = document.getElementById("submitButton");
-
-		checkMeOut.addEventListener("change", function () {
-			if (checkMeOut.checked) {
-				console.log("it's checked");
-			}
-			else {
-				console.log("it's unchecked");
-			}
-		}); // end addEventListener
-		
-		submitButton.addEventListener("click", function(event){
-			event.preventDefault();
-			console.log("Email: " + email.value);
-			console.log("Password: " + password.value);
-			
-			
-			
-		});
-		
-		
-	} // end else if
 
 })();
 
